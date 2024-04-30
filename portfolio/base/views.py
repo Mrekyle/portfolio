70243585325de19b9484b90bccce69d57b84c548
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2024-04-30 16:34:41 +0100

    Added contact form function. Send mail set up to display in console. Added information for bio

--- portfolio/base/views.py
+++ portfolio/base/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib import messages
+
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -6,6 +10,47 @@ def index(request):
     """
         Index page
     """
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        name = request.POST.get('name')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        subject_line = f"New contact request from {name}"
+        send_copy = request.POST.get('send_copy')
+
+        contact_email = f"""
+                New contact request from : {name}. 
+
+                ====================================
+                Subject : {subject}.
+
+                {message}.
+
+                ====================================
+
+                You can contact them at: {email}
+            """
+
+        if send_copy == 'on':
+            send_mail(
+                subject=subject_line,
+                message=contact_email,
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[email]
+            )
+            messages.success(request, f"Thank you {name}. Your email was sent successfully.")
+            return redirect('index')
+        else:
+            send_mail(
+                subject=subject_line,
+                message=contact_email,
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[email]
+            )
+            messages.success(request, f"Thank you {name}. Your email was sent successfully.")
+            return redirect('index')
+
 
     template = 'index.html'
 
